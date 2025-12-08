2ba3d29e842290c2a9b4dff4879d7ffb9fc842fa
elnaxy12 <aryagilang533@gmail.com>
2025-12-08 11:46:21 +0700

    git init

--- resources/views/v_login/v_forgot/app.blade.php
+++ resources/views/v_login/v_forgot/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Tab Login | Adidas</title>
+    <title>Reset Password | Adidas</title>
 
     <link rel="icon"
         href='data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M23.5 18h-5.298a.5.5 0 0 1-.423-.233l-5.535-8.775a.499.499 0 0 1 .175-.701l3.888-2.225a.5.5 0 0 1 .671.167l6.945 11A.5.5 0 0 1 23.5 18zm-5.022-1h4.115l-6.205-9.828-3.02 1.728 5.11 8.1zm-3.463 1H9.721a.502.502 0 0 1-.423-.233L6.23 12.909a.499.499 0 0 1 .175-.701l3.892-2.229a.5.5 0 0 1 .671.167l4.47 7.086a.5.5 0 0 1-.423.768zm-5.019-1h4.112l-3.731-5.915-3.022 1.731L9.996 17zm-3.604 1H1.095a.5.5 0 0 1-.423-.233l-.595-.944a.5.5 0 0 1 .175-.701l3.892-2.224a.5.5 0 0 1 .671.167l2 3.168a.5.5 0 0 1-.423.767zm-5.021-1h4.115l-1.261-1.997-3.023 1.728.169.269z"/></svg>'
@@ -24,79 +24,26 @@
                     {{-- head form --}}
                     <div class="flex items-center justify-center md:h-[200px] h-[150px]">
                         <h2 class="text-white font-sans font-semibold md:text-4xl text-3xl select-none cursor-default">
-                            Login</h2>
+                            Reset Password</h2>
                     </div>
 
-                    <form action="{{ route('login.post') }}" method="POST">
+                    <form action="{{ route('password.email') }}" method="POST">
                         @csrf
-
-                        <div class="flex flex-col gap-5 items-center">
-
-                            {{-- Menampilkan error login --}}
-                            @if(session('error'))
-                                <div
-                                    class="text-red-400 text-xs text-center md:w-xs w-[14rem] absolute translate-y-[-35px]">
-                                    {{ session('error') }}
-                                </div>
+                        <div class="flex flex-col gap-16 items-center">
+                            <label>
+                                <input
+                                    class="md:w-xs w-[14rem] md:text-sm text-xs p-2 text-white placeholder-white/50 focus:outline-none border-b focus:ring-white/50"
+                                    type="email" placeholder="Email" name="email" required>
+                            </label>
+                            @if (session('status'))
+                                <p class="text-white text-xs absolute bottom-10">{{ session('status') }}</p>
                             @endif
-
-                            {{-- email --}}
-                            <div>
-                                <label>
-                                    <input
-                                        class="md:w-xs w-[14rem] md:text-sm text-xs p-2 text-white placeholder-white/50 focus:outline-none border-b focus:ring-white/50"
-                                        type="email" placeholder="Email" name="email" value="{{ old('email') }}"
-                                        required>
-                                </label>
-
-                                @error('email')
-                                    <p class="text-red-400 text-xs mt-1">{{ $message }}</p>
-                                @enderror
-                            </div>
-
-                            {{-- password --}}
-                            <div>
-                                <label>
-                                    <input
-                                        class="md:w-xs w-[14rem] md:text-sm text-xs p-2 text-white placeholder-white/50 focus:outline-none border-b focus:ring-white/50"
-                                        type="password" placeholder="Password" name="password" required>
-                                </label>
-
-                                @error('password')
-                                    <p class="text-red-400 text-xs mt-1">{{ $message }}</p>
-                                @enderror
-                            </div>
-
-                            {{-- remember me --}}
-                            <div class="flex md:justify-start md:w-xs w-[14rem]">
-                                <label for="check" class="flex gap-1 cursor-pointer">
-                                    <input id="check" class="scale-75 cursor-pointer" type="checkbox" name="remember" value="1">
-                                    <span class="text-white text-xs select-none font-thin m-auto h-full">
-                                        Remember me
-                                    </span>
-                                </label>
-
-                                @error('remember')
-                                    <p class="text-red-400 md:text-xs text-[10px] mt-1 absolute md:translate-y-4 translate-y-3">{{ $message }}</p>
-                                @enderror
-                            </div>
-
-                            {{-- button login --}}
-                            <div>
-                                <button
-                                    class="bg-white md:w-3xs w-[10rem] md:p-2 p-[4px] rounded-2xl md:mt-5 cursor-pointer">
-                                    Login
-                                </button>
-                            </div>
-
-                            {{-- forgot password --}}
-                            <div class="mb-2">
-                                <a class="text-white text-xs hover:text-blue-900" href="{{ route('password.request') }}">
-                                    Forgot Password?
-                                </a>
-                            </div>
+                            <button
+                                class="bg-white md:w-3xs w-[10rem] md:p-2 p-[4px] rounded-2xl md:mt-5 cursor-pointer">
+                                Send Email</button>
                         </div>
                     </form>
+
                 </div>
             </div>
 
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\ForgotPasswordController;
+use App\Http\Controllers\Auth\ResetPasswordController;
 use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\CartController;
@@ -26,6 +28,15 @@ Route::post('/register', [RegisterController::class, 'register']);
 Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [LoginController::class, 'login'])->name('login.post');
 Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
+
+// Forgot Password (request reset link)
+Route::get('/forgot-password', [ForgotPasswordController::class, 'showLinkRequestForm'])->name('password.request');
+Route::post('/forgot-password', [ForgotPasswordController::class, 'sendResetLinkEmail'])->name('password.email');
+
+// Reset Password (form password baru)
+Route::get('/reset-password/{token}', [ResetPasswordController::class, 'showResetForm'])->name('password.reset');
+Route::post('/reset-password', [ResetPasswordController::class, 'reset'])->name('password.update');
+
 
 // User routes (AUTH)
 Route::middleware('auth')->group(function () {
